37a12f4f0761b268528f775865b7bf750015e235
Vahe <vahe.ohanyan.techx@gmail.com>
2025-12-15 17:37:01 +0400

    Added

---
+++ index.ts
@@ -0,0 +1,22 @@
+// Task1
+function calculateArea(height: number, width: number): number {
+  return width * height;
+}
+console.log(calculateArea(20, 30));
+
+const foo = (...nums: number[]): number => {
+  return nums.reduce((x, y) => x + y);
+};
+console.log(foo(1, 2, 3, 4, 5));
+
+enum daysOfWeek {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+let dayIndex: number = new Date().getDay();
+console.log(daysOfWeek[dayIndex]);
